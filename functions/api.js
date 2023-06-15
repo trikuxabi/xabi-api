b28c167cb2caf71f4f54ca16f421e333b6c0c60a
Xabi Linazasoro <xa2@Xabis-MacBook-Pro.local>
2023-06-15 12:36:47 +0200

    database

--- functions/api.js
+++ functions/api.js
@@ -3,42 +3,24 @@ const serverless = require("serverless-http");
 const app = express ();
 const router = express.Router();
 const fs = require("fs");
-const data = require("../json/peek.json");
+const FaunaService = require('@brianmmdev/faunaservice');
+const service = new FaunaService(process.env.DB_KEY);
 
 app.use(express.json());
 
 
 router.get('/', (req, res) => {
 	
-	// res.json(data);
-
-	res.send(process.env.XABI_API);
+	var data = await service.listRecords("music");
+	res.json(data);
 
 });
 
 router.post('/', (req, res) => {
 	
-	/*
-	const jsonObj = req.query;
-
-	fs.writeFile(
-		"./json/peek.json",
-		JSON.stringify(jsonObj),
-		function(err) {
-			if (err) {
-				return console.log(err);
-			}
-			console.log("JSON updated.");
-		}
-	);
-
-	res.json(jsonObj);
-	*/
-
-	const jsonObj = req.query;
-	const jsonStr = JSON.stringify(jsonObj)
-	process.env.XABI_API = jsonStr;
-	res.send(process.env.XABI_API);
+	var data = req.query;
+	var created = await service.createRecord("music", data);
+	res.json(created);
 
 })
 
